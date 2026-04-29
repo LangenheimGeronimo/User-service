0e4d1995d43625d5c87d8d2e5d6615bdbd09470a
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-04-29 00:09:27 -0300

    Refactor: add logging to ApplicationConfig

--- src/main/java/com/example/userservice/config/ApplicationConfig.java
+++ src/main/java/com/example/userservice/config/ApplicationConfig.java
@@ -1,7 +1,8 @@
 package com.example.userservice.config;
 
-
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,10 +17,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
     private final UserDetailsService userDetailsService;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
+        logger.info("Configurando DaoAuthenticationProvider con BCryptPasswordEncoder");
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder()); 
@@ -35,5 +38,4 @@ public class ApplicationConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
 }
