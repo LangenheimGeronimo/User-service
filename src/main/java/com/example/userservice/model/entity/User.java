de09eb15ea8199c6e6e322e59e2d1226d19f51a3
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-19 12:50:55 -0300

    Add JPA column constraints and safe Lombok configuration in User entity

--- src/main/java/com/example/userservice/model/entity/User.java
+++ src/main/java/com/example/userservice/model/entity/User.java
@@ -11,7 +11,8 @@ import lombok.*;
 
 @Entity
 @Table(name = "users")
-@Data 
+@Getter
+@Setter
 @NoArgsConstructor 
 @AllArgsConstructor 
 @Builder 
@@ -21,25 +22,37 @@ public class User extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(nullable = false, length = 50)
     private String firstName;
+    @Column(nullable = false, length = 50)
     private String lastName;
+    @Column(nullable = false)
     private LocalDate birthDate;
-    
-    @Column(nullable = false, unique = true) 
+    @Column(nullable = false, unique = true, length = 100) 
     private String email;
-    
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role; 
-    
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private State state; 
-    
+    @Column(nullable = false)
     private String password;
-
     @Transient 
     @Builder.Default 
     private List<Long> orderIds = new ArrayList<>();
-
     @Column(name = "ban_until")
     private LocalDateTime banUntil;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id != null && id.equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
