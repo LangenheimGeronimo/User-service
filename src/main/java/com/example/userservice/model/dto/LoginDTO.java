7242e1dcf5a3b40661928095e48c0907587fd30a
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-19 22:08:01 -0300

    Refactor: validation logic and fix typos in LoginDTO

--- src/main/java/com/example/userservice/model/dto/LoginDTO.java
+++ src/main/java/com/example/userservice/model/dto/LoginDTO.java
@@ -3,17 +3,15 @@ package com.example.userservice.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+
 
 public record LoginDTO(
         @Schema(description = "Correo electrónico del usuario", example = "geronimo@email.com", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank(message = "Debe ingresar un corre valido")
+        @NotBlank(message = "Debe ingresar un correo válido")
         @Email(message = "Debe ser una dirección de correo electrónico con formato correcto")
         String email,
         @Schema(description = "Contraseña del usuario", example = "mipassword1234", format = "password", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank(message = "Debe ingresar una contraseña valida")
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",  
-                 message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial")
+        @NotBlank(message = "Debe ingresar una contraseña válida")
         String password
     ) {}
 
