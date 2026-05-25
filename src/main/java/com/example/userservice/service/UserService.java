257e850c66fbc89aeb66fc4fa824306b38c2bbf5
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-24 23:04:01 -0300

    Add Lombok annotations and Slf4j logging refactor to UserService

--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -2,9 +2,11 @@ package com.example.userservice.service;
 
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.specification.UserSpecifications;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.transaction.annotation.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,25 +22,18 @@ import com.example.userservice.mapper.*;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
 public class UserService {
 	
 	private final UserRepository userRepository;
 	private final UserMapper userMapper;
 	private final PasswordEncoder passwordEncoder;
-
-
-	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-	
-	public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-		this.passwordEncoder = passwordEncoder;
-    }
 	
 	//POST
 	@Transactional
 	public UserResponseDTO createUser(UserCreateDTO dto) { 
-		logger.info("Intento de registro para el usuario con email: {}", dto.email());
+		log.info("Intento de registro para el usuario con email: {}", dto.email());
 		if(userRepository.existsByEmail(dto.email())) {
 			throw new EmailAlreadyExistsException();
 		}
@@ -47,13 +42,13 @@ public class UserService {
 		userEntity.setPassword(passwordEncoder.encode(dto.password()));
 
 		User savedUser = userRepository.save(userEntity);	
-		logger.info("Usuario guardado exitosamente: {}", savedUser.getId());
+		log.info("Usuario guardado exitosamente: {}", savedUser.getId());
 		return userMapper.toResponseDto(savedUser);
 	}
 	
 	//GET
 	public UserResponseDTO getUser(Long idUser) {
-		logger.info("Intento de obtener por id: {}", idUser);
+		log.info("Intento de obtener por id: {}", idUser);
 		User user = userRepository.findById(idUser).orElseThrow(UserNotFoundException::new);
 	    return userMapper.toResponseDto(user);
 	}
@@ -61,7 +56,7 @@ public class UserService {
 	// PUT
 	@Transactional
 	public UserResponseDTO editUser(Long idUser, UserUpdateDTO dto) {
-		logger.info("Intento de editar un usuario por id: {}", idUser);
+		log.info("Intento de editar un usuario por id: {}", idUser);
 		
 		User user = userRepository.findById(idUser).orElseThrow(UserNotFoundException::new);
 		
@@ -73,14 +68,14 @@ public class UserService {
 
 		User updatedUser = userRepository.save(user);
 		
-		logger.info("Usuario editado y guardado correctamente: {}", updatedUser.getId());
+		log.info("Usuario editado y guardado correctamente: {}", updatedUser.getId());
 		return userMapper.toResponseDto(updatedUser);
 	}
 
 	//DELETE
 	@Transactional
 	public void deleteUser(Long idUser) {
-		logger.info("Intento de borrar un usuario por id: {}", idUser);
+		log.info("Intento de borrar un usuario por id: {}", idUser);
 	    User user = userRepository.findById(idUser).orElseThrow(UserNotFoundException::new);
 
 	    if(user.getState() == (State.DELETED)) {
@@ -88,13 +83,13 @@ public class UserService {
 	    }
 
 	    user.setState(State.DELETED);
-		logger.info("Usuario eliminado correctamente: {}", idUser);
+		log.info("Usuario eliminado correctamente: {}", idUser);
     	userRepository.save(user);
 	}
 	
 	//GET
 	public State getState(Long idUser) {	
-		logger.info("Intento de obtener un usuario por id: {}", idUser);
+		log.info("Intento de obtener un usuario por id: {}", idUser);
 		User user = userRepository.findById(idUser).orElseThrow(UserNotFoundException::new);
 		State userState = user.getState();
 		return userState;
@@ -102,26 +97,26 @@ public class UserService {
 	
 	//GET/email
 	public UserResponseDTO getUserByEmail(String email) {
-		logger.info("Intento de obtener un usuario por email: {}", email);
+		log.info("Intento de obtener un usuario por email: {}", email);
 	    User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
-		logger.info("Usuario encontrado con email: {}", email);
+		log.info("Usuario encontrado con email: {}", email);
 	    return userMapper.toResponseDto(user);
 	}
 
 	//EXTRA
 	@Transactional
 	public void changeState(Long idUser, State newState) {
-		logger.info("Intento de cambiar de estado de un usuario por id: {}", idUser);
+		log.info("Intento de cambiar de estado de un usuario por id: {}", idUser);
 		User user = userRepository.findById(idUser).orElseThrow(UserNotFoundException::new);
 		
 		user.setState(newState);
 		userRepository.save(user);
-		logger.info("Estado actualizado correctamente a {} para el usuario {}", newState, idUser);
+		log.info("Estado actualizado correctamente a {} para el usuario {}", newState, idUser);
 	}
 
 	//GET
 	public Page<UserResponseDTO> getUsers(String firstName, String lastName, String email, State state, Pageable pageable) {
-		logger.info("Búsqueda avanzada de usuarios con filtros dinámicos");
+		log.info("Búsqueda avanzada de usuarios con filtros dinámicos");
 
 		Specification<User> spec = Specification.where(UserSpecifications.hasFirstName(firstName))
 												.and(UserSpecifications.hasLastName(lastName))
