09b390908d512f6e4465133d1747ebbcac6a56c1
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:02:37 -0300

    Refactor: message to static constant and add flexible constructor in InvalidStateException

--- src/main/java/com/example/userservice/exception/InvalidStateException.java
+++ src/main/java/com/example/userservice/exception/InvalidStateException.java
@@ -1,7 +1,17 @@
 package com.example.userservice.exception;
 
 public class InvalidStateException extends IllegalArgumentException {
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "El estado enviado no es válido para esta operación";
+
+    //Constructor por defecto
     public InvalidStateException() {
-        super("El estado enviado no es válido para esta operación.");
+        super(DEFAULT_MESSAGE);
     }
+
+    // Constructor flexible 
+    public InvalidStateException(String message) {
+        super(message);
+    }
+
 }
