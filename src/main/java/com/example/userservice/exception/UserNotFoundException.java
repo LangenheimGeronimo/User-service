93449762d4f9d951b466f151a020d79c2389b093
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:17:46 -0300

    Refactor: message to static constant and add flexible constructor in UserNotFoundException

--- src/main/java/com/example/userservice/exception/UserNotFoundException.java
+++ src/main/java/com/example/userservice/exception/UserNotFoundException.java
@@ -1,10 +1,16 @@
 package com.example.userservice.exception;
 
 public class UserNotFoundException extends RuntimeException {
+
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "Usuario no encontrado";
+
+    //Constructor por defecto
     public UserNotFoundException() {
-        super("Usuario no encontrado.");
+        super(DEFAULT_MESSAGE);
     }
 
+    // Constructor flexible 
     public UserNotFoundException(String message) {
         super(message);
     }
