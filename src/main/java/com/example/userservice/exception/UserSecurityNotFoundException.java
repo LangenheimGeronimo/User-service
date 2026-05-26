8ce2b34a0b9d4cbbdd836d2227bd122d279bee6d
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:22:50 -0300

    Refactor: message to static constant and add flexible constructor in UserSecurityNotFoundException

--- src/main/java/com/example/userservice/exception/UserSecurityNotFoundException.java
+++ src/main/java/com/example/userservice/exception/UserSecurityNotFoundException.java
@@ -3,7 +3,17 @@ package com.example.userservice.exception;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserSecurityNotFoundException extends UsernameNotFoundException {
+
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "Security: User not found in system";
+
+    //Constructor por defecto
     public UserSecurityNotFoundException() {
-        super("Security: User not found in system");
+        super(DEFAULT_MESSAGE);
+    }
+
+    // Constructor flexible 
+    public UserSecurityNotFoundException(String message) {
+        super(message);
     }
 }
