ff7eff0baf08ebade3b0e4d3e029ec9efe37349e
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 08:52:22 -0300

    Refactor: message to static constant and add flexible constructor in InvalidLoginException

--- src/main/java/com/example/userservice/exception/InvalidLoginException.java
+++ src/main/java/com/example/userservice/exception/InvalidLoginException.java
@@ -3,7 +3,16 @@ package com.example.userservice.exception;
 import org.springframework.security.authentication.BadCredentialsException;
 
 public class InvalidLoginException extends BadCredentialsException {
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "Usuario o contraseña incorrectos.";
+
+    //Constructor por defecto
     public InvalidLoginException() {
-        super("Usuario o contraseña incorrectos");
+        super(DEFAULT_MESSAGE);
+    }
+
+    // Constructor flexible 
+    public InvalidLoginException(String message) {
+        super(message);
     }
 }
