bc4558a384e0095bba46a214e5d3c1fb918a86f2
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:11:20 -0300

    Refactor: message to static constant and add flexible constructor in SelfReportException

--- src/main/java/com/example/userservice/exception/SelfReportException.java
+++ src/main/java/com/example/userservice/exception/SelfReportException.java
@@ -1,8 +1,16 @@
 package com.example.userservice.exception;
 
 public class SelfReportException extends RuntimeException{
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "Un usuario no puede denunciarse a sí mismo";
+
+    //Constructor por defecto
     public SelfReportException() {
-        super("Un usuario no puede denunciarse a sí mismo.");
+        super(DEFAULT_MESSAGE);
+    }
+
+    // Constructor flexible 
+    public SelfReportException(String message) {
+        super(message);
     }
 }
-//
