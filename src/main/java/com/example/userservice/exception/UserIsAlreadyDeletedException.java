e5b2f6c4dfd0b223d3c5a64aac409f96477b5b94
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:16:27 -0300

    Refactor: message to static constant and add flexible constructor in UserIsAlreadyDeletedException

--- src/main/java/com/example/userservice/exception/UserIsAlreadyDeletedException.java
+++ src/main/java/com/example/userservice/exception/UserIsAlreadyDeletedException.java
@@ -2,11 +2,16 @@ package com.example.userservice.exception;
 
 public class UserIsAlreadyDeletedException extends RuntimeException {
 
-    public UserIsAlreadyDeletedException(){
-        super("La cuenta se encuentra inhabilitada.");
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "La cuenta se encuentra inhabilitada";
+
+    //Constructor por defecto
+    public UserIsAlreadyDeletedException() {
+        super(DEFAULT_MESSAGE);
     }
 
-    public UserIsAlreadyDeletedException(String message){
+    // Constructor flexible 
+    public UserIsAlreadyDeletedException(String message) {
         super(message);
     }
 
