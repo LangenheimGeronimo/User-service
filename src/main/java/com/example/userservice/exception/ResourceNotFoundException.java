2319ace51274c143d07264d1bf2107cd767ad729
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 19:06:02 -0300

    Refactor: message to static constant and add flexible constructor in ResourceNotFoundException

--- src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
@@ -1,10 +1,15 @@
 package com.example.userservice.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    // Visible desde los tests
+    public static final String DEFAULT_MESSAGE = "El recurso solicitado no fue encontrado";
+
+    //Constructor por defecto
     public ResourceNotFoundException() {
-        super("El recurso solicitado no fue encontrado");
+        super(DEFAULT_MESSAGE);
     }
 
+    // Constructor flexible 
     public ResourceNotFoundException(String message) {
         super(message);
     }
