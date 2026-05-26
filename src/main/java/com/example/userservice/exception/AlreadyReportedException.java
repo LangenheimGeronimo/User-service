ac7d9a76820894de15be2ae54a78ba1bd4456578
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-05-26 08:46:25 -0300

    Refactor: message to static constant and add flexible constructor in AlreadyReportedException

--- src/main/java/com/example/userservice/exception/AlreadyReportedException.java
+++ src/main/java/com/example/userservice/exception/AlreadyReportedException.java
@@ -1,8 +1,17 @@
 package com.example.userservice.exception;
 
 public class AlreadyReportedException extends RuntimeException{
+    // Visible desde los tests.
+    public static final String DEFAULT_MESSAGE = "El usuario ya ha realizado una denuncia previa contra este perfil.";
+
+    //Constructor por defecto
     public AlreadyReportedException() {
-        super("El usuario ya ha realizado una denuncia previa contra este perfil.");
+        super(DEFAULT_MESSAGE);
+    }
+
+    // Constructor flexible 
+    public AlreadyReportedException(String message) {
+        super(message);
     }
 }
 
