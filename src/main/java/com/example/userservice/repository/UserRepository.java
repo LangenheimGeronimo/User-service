bd78e3cd884b0f5b757dff773f52e3331fd867b2
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-06-11 11:51:25 -0300

    Add remaining repository cleanup

--- src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -7,12 +7,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User>{
 	
-
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
